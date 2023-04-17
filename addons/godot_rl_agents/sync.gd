c8a50096cad2a0b2ac78e6bf46151d59331bb59d
edbeeching <edbeeching@gmail.com>
2023-04-17 16:51:48 +0200

    updates sync with support for onnx models

--- addons/godot_rl_agents/sync.gd
+++ addons/godot_rl_agents/sync.gd
@@ -2,9 +2,9 @@ extends Node
 # --fixed-fps 2000 --disable-render-loop
 @export var action_repeat := 8
 @export var speed_up = 1
-@onready var start_time = Time.get_ticks_msec()
+@export var onnx_model_path := ""
 
-var n_action_steps = 0
+@onready var start_time = Time.get_ticks_msec()
 
 const MAJOR_VERSION := "0"
 const MINOR_VERSION := "3" 
@@ -20,18 +20,116 @@ var need_to_send_obs = false
 var args = null
 var initialized = false
 var just_reset = false
+var onnx_model = null
+var n_action_steps = 0
 
+var _action_space : Dictionary
+var _obs_space : Dictionary
 
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-
 	await get_tree().root.ready
 	get_tree().set_pause(true) 
 	_initialize()
 	await get_tree().create_timer(1.0).timeout
 	get_tree().set_pause(false) 
+	
+func _initialize():
+	_get_agents()
+	_obs_space = agents[0].get_obs_space()
+	_action_space = agents[0].get_action_space()
+	args = _get_args()
+	Engine.physics_ticks_per_second = _get_speedup() * 60 # Replace with function body.
+	Engine.time_scale = _get_speedup() * 1.0
+	prints("physics ticks", Engine.physics_ticks_per_second, Engine.time_scale, _get_speedup(), speed_up)
+	
+
+	connected = connect_to_server()
+	if connected:
+		_set_heuristic("model")
+		_handshake()
+		_send_env_info()
+	elif onnx_model_path != "":
+		onnx_model = ONNXModel.new(onnx_model_path, 1)
+		_set_heuristic("model")
+	else:
+		_set_heuristic("human")  
 		
+	_set_seed()
+	_set_action_repeat()
+	initialized = true  
+
+func _physics_process(delta): 
+	# two modes, human control, agent control
+	# pause tree, send obs, get actions, set actions, unpause tree
+	if n_action_steps % action_repeat != 0:
+		n_action_steps += 1
+		return
+
+	n_action_steps += 1
+	
+	if connected:
+		get_tree().set_pause(true) 
+		
+		if just_reset:
+			just_reset = false
+			var obs = _get_obs_from_agents()
+		
+			var reply = {
+				"type": "reset",
+				"obs": obs
+			}
+			_send_dict_as_json_message(reply)
+			# this should go straight to getting the action and setting it checked the agent, no need to perform one phyics tick
+			get_tree().set_pause(false) 
+			return
+		
+		if need_to_send_obs:
+			need_to_send_obs = false
+			var reward = _get_reward_from_agents()
+			var done = _get_done_from_agents()
+			#_reset_agents_if_done() # this ensures the new observation is from the next env instance : NEEDS REFACTOR
+			
+			var obs = _get_obs_from_agents()
+			
+			var reply = {
+				"type": "step",
+				"obs": obs,
+				"reward": reward,
+				"done": done
+			}
+			_send_dict_as_json_message(reply)
+		
+		var handled = handle_message()
+	
+	elif onnx_model != null:
+		var obs : Array = _get_obs_from_agents()
+		
+		
+		
+		var actions = []
+		for o in obs:
+			var action = onnx_model.run_inference(o["obs"], 1.0)
+			var action_dict = _extract_action_dict(action["output"])
+			actions.append(action_dict)
+		_set_agent_actions(actions) 
+		need_to_send_obs = true
+		get_tree().set_pause(false) 
+		
+	else:
+		_reset_agents_if_done()	
+
+func _extract_action_dict(action_array: Array):
+	var index = 0
+	var result = {}
+	for key in _action_space.keys():
+		var size = _action_space[key]["size"]
+		result[key] = action_array.slice(index,index+size)
+		index += size
+		
+	return result
+
 func _get_agents():
 	agents = get_tree().get_nodes_in_group("AGENT")
 
@@ -76,16 +174,15 @@ func _send_dict_as_json_message(dict):
 func _send_env_info():
 	var json_dict = _get_dict_json_message()
 	assert(json_dict["type"] == "env_info")
-	
+
+		
 	var message = {
 		"type" : "env_info",
-		#"obs_size": agents[0].get_obs_size(),
-		"observation_space": agents[0].get_obs_space(),
-		"action_space":agents[0].get_action_space(),
+		"observation_space": _obs_space,
+		"action_space":_action_space,
 		"n_agents": len(agents)
 		}
 	_send_dict_as_json_message(message)
-
 
 func connect_to_server():
 	print("Waiting for one second to allow server to start")
@@ -136,70 +233,7 @@ func _set_action_repeat():
 func disconnect_from_server():
 	stream.disconnect_from_host()
 
-func _initialize():
-	_get_agents()
-	
-	args = _get_args()
-	Engine.physics_ticks_per_second = _get_speedup() * 60 # Replace with function body.
-	Engine.time_scale = _get_speedup() * 1.0
-	prints("physics ticks", Engine.physics_ticks_per_second, Engine.time_scale, _get_speedup(), speed_up)
-	
-	connected = connect_to_server()
-	if connected:
-		_set_heuristic("model")
-		_handshake()
-		_send_env_info()
-	else:
-		_set_heuristic("human")  
-		
-	_set_seed()
-	_set_action_repeat()
-	initialized = true  
 
-func _physics_process(delta): 
-	# two modes, human control, agent control
-	# pause tree, send obs, get actions, set actions, unpause tree
-	if n_action_steps % action_repeat != 0:
-		n_action_steps += 1
-		return
-
-	n_action_steps += 1
-	
-	if connected:
-		get_tree().set_pause(true) 
-		
-		if just_reset:
-			just_reset = false
-			var obs = _get_obs_from_agents()
-		
-			var reply = {
-				"type": "reset",
-				"obs": obs
-			}
-			_send_dict_as_json_message(reply)
-			# this should go straight to getting the action and setting it checked the agent, no need to perform one phyics tick
-			get_tree().set_pause(false) 
-			return
-		
-		if need_to_send_obs:
-			need_to_send_obs = false
-			var reward = _get_reward_from_agents()
-			var done = _get_done_from_agents()
-			#_reset_agents_if_done() # this ensures the new observation is from the next env instance : NEEDS REFACTOR
-			
-			var obs = _get_obs_from_agents()
-			
-			var reply = {
-				"type": "step",
-				"obs": obs,
-				"reward": reward,
-				"done": done
-			}
-			_send_dict_as_json_message(reply)
-		
-		var handled = handle_message()
-	else:
-		_reset_agents_if_done()
 
 func handle_message() -> bool:
 	# get json message: reset, step, close
