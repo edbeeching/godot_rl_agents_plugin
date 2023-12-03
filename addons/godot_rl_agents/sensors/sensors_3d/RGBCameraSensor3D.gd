428070f8ff6bb4c2e0d8b2160fc150314d3bdb51
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-12-03 12:34:13 +0100

    Update addons/godot_rl_agents/sensors/sensors_3d/RGBCameraSensor3D.gd
    Co-authored-by: Metin Celik <metincelik88@gmail.com>

--- addons/godot_rl_agents/sensors/sensors_3d/RGBCameraSensor3D.gd
+++ addons/godot_rl_agents/sensors/sensors_3d/RGBCameraSensor3D.gd
@@ -5,7 +5,7 @@ var camera_pixels = null
 @onready var camera_texture := $Control/TextureRect/CameraTexture as Sprite2D
 
 func get_camera_pixel_encoding():
-	return camera_texture.get_texture().get_image().data["data"].hex_encode()
+	return camera_texture.get_texture().get_image().get_data().hex_encode()
 
 func get_camera_shape()-> Array:
 	return [$SubViewport.size[0], $SubViewport.size[1], 3]
