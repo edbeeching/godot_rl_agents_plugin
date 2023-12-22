cf6bab7a3c5632d50781edf7e452781468c394c5
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-12-22 14:58:44 +0100

    Update RGBCameraSensor3D.gd

--- addons/godot_rl_agents/sensors/sensors_3d/RGBCameraSensor3D.gd
+++ addons/godot_rl_agents/sensors/sensors_3d/RGBCameraSensor3D.gd
@@ -3,12 +3,19 @@ class_name RGBCameraSensor3D
 var camera_pixels = null
 
 @onready var camera_texture := $Control/TextureRect/CameraTexture as Sprite2D
+@onready var sub_viewport := $SubViewport as SubViewport
+
 
 func get_camera_pixel_encoding():
 	return camera_texture.get_texture().get_image().get_data().hex_encode()
 
-func get_camera_shape()-> Array:
-	if $SubViewport.transparent_bg:
-		return [$SubViewport.size[0], $SubViewport.size[1], 4]
+
+func get_camera_shape() -> Array:
+	assert(
+		sub_viewport.size.x >= 36 and sub_viewport.size.y >= 36,
+		"SubViewport size must be 36x36 or larger."
+	)
+	if sub_viewport.transparent_bg:
+		return [4, sub_viewport.size.y, sub_viewport.size.x]
 	else:
-		return [$SubViewport.size[0], $SubViewport.size[1], 3]
+		return [3, sub_viewport.size.y, sub_viewport.size.x]
