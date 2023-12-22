d3fa229f49fcee5f1073d54545a59252b1f0b6fc
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-12-22 14:48:32 +0100

    Update RGBCameraSensor3D.gd

--- addons/godot_rl_agents/sensors/sensors_3d/RGBCameraSensor3D.gd
+++ addons/godot_rl_agents/sensors/sensors_3d/RGBCameraSensor3D.gd
@@ -8,7 +8,8 @@ func get_camera_pixel_encoding():
 	return camera_texture.get_texture().get_image().get_data().hex_encode()
 
 func get_camera_shape()-> Array:
+	assert($SubViewport.size.x >= 36 and $SubViewport.size.y >= 36, "SubViewport size must be 36x36 or larger.")
 	if $SubViewport.transparent_bg:
-		return [$SubViewport.size[0], $SubViewport.size[1], 4]
+		return [4, $SubViewport.size.y, $SubViewport.size.x]
 	else:
-		return [$SubViewport.size[0], $SubViewport.size[1], 3]
+		return [3, $SubViewport.size.y, $SubViewport.size.x]
